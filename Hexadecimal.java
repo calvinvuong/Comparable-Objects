0e8d7e9fd50d3345e1904998bc7494539e027a7d
Calvin Vuong <calvin@boxer.local>
2015-12-09 20:39:57 -0500

    Finished and debugged SuperArray; debugged everything else

--- Hexadecimal.java
+++ Hexadecimal.java
@@ -1,148 +1,148 @@
 /*
-Calvin Vuong
-APCS1 pd5
-HW45 -- Come Together
-2015-12-10
+  Calvin Vuong
+  APCS1 pd5
+  HW45 -- Come Together
+  2015-12-10
 */
 
 public class Hexadecimal implements Comparable{
 	
-	private final static String HEXDIGITS = "0123456789ABCDEF";
-	private int _decNum;
-	private String _hexNum;
+    private final static String HEXDIGITS = "0123456789ABCDEF";
+    private int _decNum;
+    private String _hexNum;
 	
-	/*=====================================
-	default constructor
-	pre:  n/a
-	post: initializes _decNum to 0, _hexNum to "0"
-	=====================================*/
-	public Hexadecimal() {
-		_decNum = 0;
-		_hexNum = "0";
-	}
+    /*=====================================
+      default constructor
+      pre:  n/a
+      post: initializes _decNum to 0, _hexNum to "0"
+      =====================================*/
+    public Hexadecimal() {
+	_decNum = 0;
+	_hexNum = "0";
+    }
 	
-	/*=====================================
-	overloaded constructor
-	pre:  n >= 0
-	post: sets _decNum to n, _hexNum to equiv string of hexadecimals
-	=====================================*/
-	public Hexadecimal( int n ) {
-		_decNum = n;
-		_hexNum = decToHex(n);
-	}
+    /*=====================================
+      overloaded constructor
+      pre:  n >= 0
+      post: sets _decNum to n, _hexNum to equiv string of hexadecimals
+      =====================================*/
+    public Hexadecimal( int n ) {
+	_decNum = n;
+	_hexNum = decToHex(n);
+    }
 	
-	/*=====================================
-	overloaded constructor
-	pre:  s is String representing non-negative hexadecimal
-	post: sets _hexNum to input, _decNum to decimal equiv
-	=====================================*/
-	public Hexadecimal( String s ) {
-		_decNum = hexToDec(s); //used because
-		_hexNum = s;
-	}
+    /*=====================================
+      overloaded constructor
+      pre:  s is String representing non-negative hexadecimal
+      post: sets _hexNum to input, _decNum to decimal equiv
+      =====================================*/
+    public Hexadecimal( String s ) {
+	_decNum = hexToDec(s); //used because
+	_hexNum = s;
+    }
 	
-	/*=====================================
-	String toString() -- returns String representation of this Object
-	pre:  n/a
-	post: returns hexadecimal representation of this Object
-	=====================================*/
-	public String toString() {
-		return _hexNum;
-	}
+    /*=====================================
+      String toString() -- returns String representation of this Object
+      pre:  n/a
+      post: returns hexadecimal representation of this Object
+      =====================================*/
+    public String toString() {
+	return _hexNum;
+    }
 
     public int getDecNum(){
 	return _decNum;
     }
 	
-	/*=====================================
-	String decToHex(int) -- converts base-10 input to a hexadecimal
-	pre:  n >= 0
-	post: returns String in hexadecimal form
-	eg  decToHex(0) -> "0"
-	decToHex(1) -> "1"
-	decToHex(2) -> "2"
-	decToHex(3) -> "3"
-	decToHex(14) -> "E"
-	=====================================*/
-	public static String decToHex( int n ) {
-		String retstr = "";
-		//uses algo discussed in class
-		while (n != 0) {
-			int remainder = n % 16;
-			String hexDigit = HEXDIGITS.substring(remainder, remainder+1); //hex equivalent to remainder
-			retstr = hexDigit + retstr; //add hexDigit to beginning of hex string
-			n = n / 16; //repeat process but with quotient
-		}
-		return retstr;
+    /*=====================================
+      String decToHex(int) -- converts base-10 input to a hexadecimal
+      pre:  n >= 0
+      post: returns String in hexadecimal form
+      eg  decToHex(0) -> "0"
+      decToHex(1) -> "1"
+      decToHex(2) -> "2"
+      decToHex(3) -> "3"
+      decToHex(14) -> "E"
+      =====================================*/
+    public static String decToHex( int n ) {
+	String retstr = "";
+	//uses algo discussed in class
+	while (n != 0) {
+	    int remainder = n % 16;
+	    String hexDigit = HEXDIGITS.substring(remainder, remainder+1); //hex equivalent to remainder
+	    retstr = hexDigit + retstr; //add hexDigit to beginning of hex string
+	    n = n / 16; //repeat process but with quotient
 	}
+	return retstr;
+    }
 	
-	/*=====================================
-	String decToHexR(int) -- converts base-10 input to a hexadecimal, recursively
-	pre:  n >= 0
-	post: returns String in hexadecimal form
-	eg  decToHexR(0) -> "0"
-	decToHexR(1) -> "1"
-	decToHexR(2) -> "2"
-	decToHexR(3) -> "3"
-	decToHexR(14) -> "E"
-	=====================================*/
+    /*=====================================
+      String decToHexR(int) -- converts base-10 input to a hexadecimal, recursively
+      pre:  n >= 0
+      post: returns String in hexadecimal form
+      eg  decToHexR(0) -> "0"
+      decToHexR(1) -> "1"
+      decToHexR(2) -> "2"
+      decToHexR(3) -> "3"
+      decToHexR(14) -> "E"
+      =====================================*/
 	
-	public static String decToHexR( int n ) {
-		if (n == 0){ //base case
-			return "";
-		}
-		else{ //recursive case
-			int remainder = n % 16;
-			String hexDigit = HEXDIGITS.substring(remainder, remainder+1); //hex equivalent to remainder
-			return decToHexR((n / 16)) + hexDigit; //add this hex digit to end of conversion of quotient
-		}
+    public static String decToHexR( int n ) {
+	if (n == 0){ //base case
+	    return "";
 	}
-	
-	/*=====================================
-	String hexToDec(String) -- converts hexadecimal input into base-10
-	pre:  s represents non-negative hexadecimal
-	post: returns int equivalent in base-10
-	eg
-	hexToDec("0") -> 0
-	hexToDec("1") -> 1
-	hexToDec("2") -> 2
-	hexToDec("3") -> 3
-	hexToDec("E") -> 14
-	=====================================*/
-	public static int hexToDec( String s ) {
-		int retInt = 0;
-		for (int i = 0 ; i < s.length() ; i++) { //iterate left to right
-			String hexDigit = s.substring(i, i + 1); //digit currently iterating over
-			int hexValue = HEXDIGITS.indexOf(hexDigit); //value of hexDigit in base10
-			retInt += hexValue * Math.pow(16, s.length()-1 - i);
-		}
-		return retInt;
+	else{ //recursive case
+	    int remainder = n % 16;
+	    String hexDigit = HEXDIGITS.substring(remainder, remainder+1); //hex equivalent to remainder
+	    return decToHexR((n / 16)) + hexDigit; //add this hex digit to end of conversion of quotient
 	}
+    }
 	
-	/*=====================================
-	String hexToDecR(String) -- converts hexadecimal input into base-10, recursively
-	pre:  s represents non-negative Hexadecimal number
-	post: returns int equivalent in base-10
-	eg
-	hexToDecR("0") -> 0
-	hexToDecR("1") -> 1
-	hexToDecR("2") -> 2
-	hexToDecR("3") -> 3
-	hexToDecR("E") -> 14
-	=====================================*/
-	public static int hexToDecR(String s) {
-		if (s.length() == 0){ //base case
-			return 0;
-		}
-		else{ //recursive case
-			String hexDigit = s.substring(0,1); //first hex digit
-			//val of digit
-			int hexInBase10 = HEXDIGITS.indexOf(hexDigit)
-			* (int) Math.pow(16, s.length()-1);
-			//return value of this digit plus value of other digits
-			return hexInBase10 + hexToDecR(s.substring(1));
-		}
+    /*=====================================
+      String hexToDec(String) -- converts hexadecimal input into base-10
+      pre:  s represents non-negative hexadecimal
+      post: returns int equivalent in base-10
+      eg
+      hexToDec("0") -> 0
+      hexToDec("1") -> 1
+      hexToDec("2") -> 2
+      hexToDec("3") -> 3
+      hexToDec("E") -> 14
+      =====================================*/
+    public static int hexToDec( String s ) {
+	int retInt = 0;
+	for (int i = 0 ; i < s.length() ; i++) { //iterate left to right
+	    String hexDigit = s.substring(i, i + 1); //digit currently iterating over
+	    int hexValue = HEXDIGITS.indexOf(hexDigit); //value of hexDigit in base10
+	    retInt += hexValue * Math.pow(16, s.length()-1 - i);
 	}
+	return retInt;
+    }
+	
+    /*=====================================
+      String hexToDecR(String) -- converts hexadecimal input into base-10, recursively
+      pre:  s represents non-negative Hexadecimal number
+      post: returns int equivalent in base-10
+      eg
+      hexToDecR("0") -> 0
+      hexToDecR("1") -> 1
+      hexToDecR("2") -> 2
+      hexToDecR("3") -> 3
+      hexToDecR("E") -> 14
+      =====================================*/
+    public static int hexToDecR(String s) {
+	if (s.length() == 0){ //base case
+	    return 0;
+	}
+	else{ //recursive case
+	    String hexDigit = s.substring(0,1); //first hex digit
+	    //val of digit
+	    int hexInBase10 = HEXDIGITS.indexOf(hexDigit)
+		* (int) Math.pow(16, s.length()-1);
+	    //return value of this digit plus value of other digits
+	    return hexInBase10 + hexToDecR(s.substring(1));
+	}
+    }
 
     /*=============================================
       boolean equals(Object) -- tells whether 2 Objs are equivalent
@@ -197,69 +197,69 @@ public class Hexadecimal implements Comparable{
     }
 	
 	
-	//main method for testing
-	public static void main( String[] args ) {
-			Binary b1 = new Binary("1101"); //13
-			Rational r1 = new Rational(26, 2); //13
-			Hexadecimal h1 = new Hexadecimal("D"); //13
+    //main method for testing
+    public static void main( String[] args ) {
+	Binary b1 = new Binary("1101"); //13
+	Rational r1 = new Rational(26, 2); //13
+	Hexadecimal h1 = new Hexadecimal("D"); //13
 			
-			Binary b2 = new Binary("111"); //7
-			Rational r2 = new Rational(7, 2); // 7/2
-			Hexadecimal h2 = new Hexadecimal("AC23D7"); //large
+	Binary b2 = new Binary("111"); //7
+	Rational r2 = new Rational(7, 2); // 7/2
+	Hexadecimal h2 = new Hexadecimal("AC23D7"); //large
 			
-			String bad = new String("hola!");
-			Binary empty = null;
+	String bad = new String("hola!");
+	Binary empty = null;
 			
-			System.out.println(h1.compareTo(r1)); //0
-			System.out.println(h1.compareTo(b1)); //0
-			System.out.println(r1.compareTo(b1)); //0
+	System.out.println(h1.compareTo(r1)); //0
+	System.out.println(h1.compareTo(b1)); //0
+	System.out.println(r1.compareTo(b1)); //0
 			
-			System.out.println(h2.compareTo(r2)); //positive
-			System.out.println(r2.compareTo(b2)); //negative
+	System.out.println(h2.compareTo(r2)); //positive
+	System.out.println(r2.compareTo(b2)); //negative
 			
-			System.out.println(h1.compareTo(bad)); //ClassCastException
-			System.out.println(h1.compareTo(empty)); //NullPointerException
+	System.out.println(h1.compareTo(bad)); //ClassCastException
+	System.out.println(h1.compareTo(empty)); //NullPointerException
 			
-			/*OLD TEST CASES...
-			System.out.println();
-			System.out.println( "Testing ..." );
+	/*OLD TEST CASES...
+	  System.out.println();
+	  System.out.println( "Testing ..." );
 			
 
-			  Hexadecimal h1 = new Hexadecimal(23);
-			  Hexadecimal h2 = new Hexadecimal("17");
-			  Hexadecimal h3 = h1;
-		Hexadecimal h4 = new Hexadecimal(100);
+	  Hexadecimal h1 = new Hexadecimal(23);
+	  Hexadecimal h2 = new Hexadecimal("17");
+	  Hexadecimal h3 = h1;
+	  Hexadecimal h4 = new Hexadecimal(100);
 		
-		System.out.println( h1 );
-		System.out.println( h2 );
-		System.out.println( h3 );
-		System.out.println( h4 );
+	  System.out.println( h1 );
+	  System.out.println( h2 );
+	  System.out.println( h3 );
+	  System.out.println( h4 );
 		
-		System.out.println( "\n==..." );
-		System.out.println( h1 == h2 ); //should be false
-		System.out.println( h1 == h3 ); //should be true
+	  System.out.println( "\n==..." );
+	  System.out.println( h1 == h2 ); //should be false
+	  System.out.println( h1 == h3 ); //should be true
 		
-		System.out.println( "\n.equals()..." );
-		System.out.println( h1.equals(h2) ); //should be true
-		System.out.println( h1.equals(h3) ); //should be true
-		System.out.println( h3.equals(h1) ); //should be true
-		System.out.println( h4.equals(h2) ); //should be false
-		System.out.println( h1.equals(h4) ); //should be false
+	  System.out.println( "\n.equals()..." );
+	  System.out.println( h1.equals(h2) ); //should be true
+	  System.out.println( h1.equals(h3) ); //should be true
+	  System.out.println( h3.equals(h1) ); //should be true
+	  System.out.println( h4.equals(h2) ); //should be false
+	  System.out.println( h1.equals(h4) ); //should be false
 		
-		System.out.println( "\n.compareTo..." );
-		System.out.println( h1.compareTo(h2) ); //should be 0
-		System.out.println( h1.compareTo(h3) ); //should be 0
-		System.out.println( h1.compareTo(h4) ); //should be neg
-		System.out.println( h4.compareTo(h1) ); //should be pos
+	  System.out.println( "\n.compareTo..." );
+	  System.out.println( h1.compareTo(h2) ); //should be 0
+	  System.out.println( h1.compareTo(h3) ); //should be 0
+	  System.out.println( h1.compareTo(h4) ); //should be neg
+	  System.out.println( h4.compareTo(h1) ); //should be pos
 		
-		System.out.println("\nTesting Recursive Methods...");
-		System.out.println( Hexadecimal.decToHexR(100) ); //64
-		System.out.println( Hexadecimal.hexToDecR("64") ); //100
+	  System.out.println("\nTesting Recursive Methods...");
+	  System.out.println( Hexadecimal.decToHexR(100) ); //64
+	  System.out.println( Hexadecimal.hexToDecR("64") ); //100
 		
-		System.out.println("\nTesting error throw...");
-		System.out.println(h1.equals(new String("hi"))); //should throw error
+	  System.out.println("\nTesting error throw...");
+	  System.out.println(h1.equals(new String("hi"))); //should throw error
 		
-		*/
-	}//end main()
+	*/
+    }//end main()
 	
 } //end class
--- SuperArray.java
+++ SuperArray.java
@@ -3,11 +3,12 @@
     HW45 -- Come Together
     2015-12-10 */
 
+//NEW METHODS FOR HW45 BELOW size()
 public class SuperArray{
      
     //~~~~~INSTANCE VARS~~~~~
     //underlying container, or "core" of this data structure:
-    private int[] _data;
+    private Comparable[] _data;
     
     //position of last meaningful value
     private int _lastPos;
@@ -19,7 +20,7 @@ public class SuperArray{
     //~~~~~METHODS~~~~~
     //default constructor --> initializes 10-item array
     public SuperArray() { 
-    	_data = new int[10];
+    	_data = new Comparable[10];
     	_lastPos = -1; //flag to indicate no lastpos yet
 	_size = 0;	
     }
@@ -43,7 +44,7 @@ public class SuperArray{
     		
     //double capacity of this SuperArray
     private void expand() {
-    	int[] temp = new int[ _data.length * 2 ];
+    	Comparable[] temp = new Comparable[ _data.length * 2 ];
     	for( int i = 0; i < _data.length; i++ ){
     	    temp[i] = _data[i];
     	}
@@ -52,15 +53,15 @@ public class SuperArray{
     
     		
     //accessor -- return value at specified index
-    public int get( int index ) { 
+    public Comparable get( int index ) { 
 	return _data[index];
     }
     
     		
     //mutator -- set value at index to newVal, 
     //           return old value at index
-    public int set( int index, int newVal ) { 
-	int temp = _data[index];
+    public Comparable set( int index, Comparable newVal ) { 
+	Comparable temp = _data[index];
 	_data[index] = newVal;
 	return temp;
     }
@@ -68,7 +69,7 @@ public class SuperArray{
     
     // ~~~~~~~~~~~~~~ PHASE II ~~~~~~~~~~~~~~
     //adds an item after the last item
-    public void add( int newVal ) { 
+    public void add( Comparable newVal ) { 
         if (_lastPos == _data.length - 1){ //if no room to add...
             expand(); //expand to make room
         }
@@ -79,12 +80,12 @@ public class SuperArray{
     
     //inserts an item at index
     //shifts existing elements to the right
-    public void add( int index, int newVal ) {
+    public void add( int index, Comparable newVal ) {
 	if (index <= _size){ //index within bounds...
 	    if (_lastPos == _data.length - 1){ //if no room to add...
 		expand(); //expand to make room
 	    }
-	    int displaced = 0; //value of displaced element
+	    Comparable displaced = null; //value of displaced element
 	    //for every index up to lastPos, and one index after
 	    //to account for one more element added
 	    for (int i = 0; i <= _lastPos + 1; i++){
@@ -94,7 +95,7 @@ public class SuperArray{
 		}
 		else if (i > index){ //if after shift
 		    //value of i becomes the next displaced
-		    int newDisplaced = _data[i]; 
+		    Comparable newDisplaced = _data[i]; 
 		    _data[i] = displaced; //overwrite with previous displaced
 		    displaced = newDisplaced; //update displaced
 		}
@@ -121,11 +122,72 @@ public class SuperArray{
     
     //return number of meaningful items in _data
     public int size() { return _size; }
+
     
+    //return index of first occurrence of Comparable Object c
+    public int linSearch(Comparable c){
+	for (int i = 0; i < _size; i++){ //iterate left to right
+	    if ( _data[i].equals(c) ) { //if Comparable at index i is identical to Comparable c...
+		return i; //return index
+	    }
+	}
+	//loop exits if no match; return -1 in this case
+	return -1;
+    }
+
+    //returns true if Comparables in _data is sorted from smallest to largest
+    //false otherwise
+    public boolean isSorted(){
+	for (int i = 0; i < _size-1; i++){ //iterate left to right up to and including 2nd to last Comparable
+	    if ( _data[i].compareTo(_data[i+1]) > 0 ){ //if value of Comparable at this index is greater than Comparable at next index...
+		return false; //the list is not sorted
+	    }
+	}
+	return true;
+    }
+
     //main method for testing
     public static void main( String[] args ) { 
 
-	//NEW TEST CASES; TESTS ListInt INTERFACE
+	//NEW TEST CASES; TESTS linSearch() and isSorted()
+	SuperArray a = new SuperArray();
+	a.add(new Rational(0,1)); //0
+	a.add(new Binary("1")); //1
+	a.add(new Rational(5,5)); //1
+	a.add(new Hexadecimal("3")); //3
+	a.add(new Binary("11")); //3
+	a.add(new Rational(10, 2)); //5
+	a.add(new Hexadecimal("F")); //15
+	a.add(new Rational(30,1));//30
+	a.add(new Binary("101000")); //40
+	a.add(new Binary("111111")); //63
+	a.add(new Hexadecimal("CDE45")); //843333
+
+	System.out.println(a);
+	
+	System.out.println(a.linSearch( new Binary("1111") )); //6
+	System.out.println(a.linSearch( new Binary("100") )); //-1
+	System.out.println(a.linSearch( new Hexadecimal("1") )); //1
+	System.out.println(a.linSearch( new Rational(9, 3) )); //3
+
+	System.out.println(a.isSorted()); //true
+
+	SuperArray b = new SuperArray();
+	b.add(new Rational(0,1)); //0
+	b.add(new Binary("1")); //1
+	b.add(new Rational(5,5)); //1
+	b.add(new Hexadecimal("3")); //3
+	b.add(new Binary("11")); //3
+	b.add(new Rational(1, 2)); //.5
+	b.add(new Hexadecimal("F")); //15
+	b.add(new Rational(30,1));//30
+	b.add(new Binary("101000")); //40
+	b.add(new Binary("111111")); //63
+	b.add(new Hexadecimal("CDE45")); //843333
+	
+	System.out.println(b.isSorted()); //false
+
+	/*OLD TEST CASES; TESTS LISTINT interface
 	ListInt calvin = new SuperArray();
 	System.out.println(calvin); // []
 	calvin.add(0);
